23a0185aef77eed341a538c1ef502df638390a5f
LagrouCedric <71886193+LagrouCedric@users.noreply.github.com>
2022-12-07 14:33:26 +0100

    changed import

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, BatchNormalization, Conv2D, MaxPooling2D, Flatten, Activation, Dropout
 from tensorflow.keras.utils import to_categorical
 
-# from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 from typing import List
 
 def getTargets(filepaths: List[str]) -> List[str]:
