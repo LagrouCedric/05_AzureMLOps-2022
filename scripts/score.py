bd3ac6c4959b8492f75585094c042746cd988bf4
LagrouCedric <71886193+LagrouCedric@users.noreply.github.com>
2022-12-07 16:55:15 +0100

    changed deployment & score

--- scripts/score.py
+++ scripts/score.py
@@ -5,14 +5,14 @@ from tensorflow import keras
 from tensorflow.keras.models import load_model
 from PIL import Image
 
-ANIMALS = ['Cat', 'Dog', 'Panda']
+OPTIONS = ['vehicle', 'non-vehicle']
 
 def init():
     global model
 
     # The AZUREML_MODEL_DIR environment variable indicates
     # a directory containing the model file you registered.
-    model_path = os.path.join(os.environ.get('AZUREML_MODEL_DIR'), 'animal-cnn')
+    model_path = os.path.join(os.environ.get('AZUREML_MODEL_DIR'), 'carDetection-cnn')
 
     model = load_model(model_path)
 
@@ -24,4 +24,4 @@ def run(image):
     predictions = model.predict(images_to_predict)
     classifications = predictions.argmax(axis=1)
 
-    return ANIMALS[classifications.tolist()[0]]
+    return OPTIONS[classifications.tolist()[0]]
--- steps/03_Deployment.py
+++ steps/03_Deployment.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 # This line read those in.
 load_dotenv()
 
-ANIMALS = os.environ.get('ANIMALS').split(',')
+OPTIONS = os.environ.get('OPTIONS').split(',')
 MODEL_NAME = os.environ.get('MODEL_NAME')
 LOCAL_DEPLOYMENT = os.environ.get('LOCAL_DEPLOYMENT')
 print(LOCAL_DEPLOYMENT)
